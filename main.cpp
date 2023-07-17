637d05c5fababdb770589889c63544fba2e77e3c
Alex Bogushenko <bogushenkoas@ukr.net>
2023-07-17 13:22:17 +0300

    1 commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+class Track
+{
+    public: 
+
+    string name;
+    tm creationdate;
+    int duration;
+
+};
+
+int main()
+{
+ time_t t = time(NULL);
+ tm local = *localtime(&t);
+ cin >> get_time(&local, "%H:%M");
+ cout << local.tm_hour << " and " << local.tm_min;
+}
